63a6637fa9a3aa06e182cf3048fc83a9e740ee14
mcrowder65 <mcrowder65@gmail.com>
2015-11-17 08:16:00 -0700

    added basic login

--- app/js/app.js
+++ app/js/app.js
@@ -14,8 +14,7 @@ var App = React.createClass({
 
               </div>
               <div className="nav navbar-nav navbar-right" id="bs-example-navbar-collapse-1">
-                <a className="navbar-brand" href="/">Log in</a>
-                
+                <li><Link to="signIn">Sign in</Link></li>
                 <li><Link to="signUp">Sign up</Link></li>
                 
               </div>
@@ -29,25 +28,63 @@ var App = React.createClass({
     );
   }
 });
-
-var Home = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Put your home page here</p>
-      </div>
-    );
-  }
-});
 var formStyle = 
 {
   textAlign: 'center'
 };
+var signIn = React.createClass
+({
+  getInitialState: function() 
+  {
+    return {userName: ''}, {password: ''};
+  },
+  handleChange: function(event) 
+  {
+    if(event.target.name == "userName")
+    {
+      this.setState({userName: event.target.value});
+      console.log(event.target.value);
+    }
+    else if(event.target.name == "password")
+    {
+      this.setState({password: event.target.value});
+      console.log(event.target.value)
+    }
+  },
+  handleClick: function(event)
+  {
+    if(this.state.password != this.state.confirmPassword)
+    {
+       alert("PASSWORDS ARE DIFFERENT");
+    }
+    else if(this.state.password.length < 8)
+    {
+      alert("Your password must be greater than 7 characters");
+    }
+  },
+  render: function() {
+    var userName = this.state.userName;
+    var password = this.state.password;
+    return (
+       <div style={formStyle}>
+          Username: <br/><input type="text" name="userName" value ={userName} onChange={this.handleChange}/><br/><br/>
+          Password: <br/><input type="password" name="password" value ={password} onChange={this.handleChange}/><br/>
+          <br/>Click <a href="#"> here</a> if you forgot your password. <br/>
+          <br/><button onClick={this.handleClick}>
+            SIGN IN
+            </button>
+      </div>
+
+
+      );
+  }
+});
+
 function hello()
 {
   console.log('hello');
 }
+
 var signUp = React.createClass
 ({
   getInitialState: function() 
@@ -78,7 +115,7 @@ var signUp = React.createClass
     {
        alert("PASSWORDS ARE DIFFERENT");
     }
-    if(this.state.password.length < 8)
+    else if(this.state.password.length < 8)
     {
       alert("Your password must be greater than 7 characters");
     }
@@ -113,7 +150,8 @@ var routes = (
       <Router>
         <Route name="app" path="/" component={App}>
           <Route name="signUp" path="/signUp" component={signUp} />
-          <Route path="*" component={Home}/>
+
+          <Route name="signIn" path="/signIn" component={signIn}/>
         </Route>
       </Router>
 );
