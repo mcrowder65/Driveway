160e193474824a5739e4db9c401bc83b6e9518ff
Matt Crowder <mcrowder65@gmail>
2015-12-01 16:13:54 +0000

    test for brennen

--- app/bundle.js
+++ app/bundle.js
@@ -208,7 +208,7 @@
 	      return {address: address, numCars: numCars, zip: zip, city: city, state: state, editing: true};
 	    }
 	    else
-	        return {address: '', numCars: '1', zip:'', city: '', state:'AL', editing: false};
+	        return {address: '', numCars: '1', zip:'', city: '', state:'AL', editing: false, time: []};
 	  },
 	  handleChange: function(event) 
 	  {
@@ -226,6 +226,13 @@
 	  handleClick: function(event)
 	  {
 	    drivewayDAO.add(localStorage.username, this.state.address, this.state.numCars, this.state.city, this.state.zip, this.state.state);
+	  },
+	  addNewTime: function()
+	  {
+	     var node = document.getElementById("time");
+	var textnode = document.createTextNode("Water");         // Create a text node
+	node.appendChild(textnode);                              // Append the text to <li>
+	document.getElementById("time").appendChild(node); 
 	  },
 	  remove: function()
 	  {
@@ -309,10 +316,125 @@
 	                        React.createElement("option", {value: "2"}, "2"), 
 	                        React.createElement("option", {value: "3"}, "3")
 	                      ), 
-	        React.createElement("br", null), React.createElement("br", null), React.createElement("button", {onClick: this.handleClick}, 
-	        "Submit"
-	        )
-	      )
+
+	          React.createElement("br", null), React.createElement("br", null), 
+	          "Day: ", React.createElement("space", null, " "), " ", React.createElement("select", {name: "day", id: "day"}, 
+	                                  React.createElement("option", {value: ""}, " "), 
+	                                  React.createElement("option", {value: "monday"}, " Monday"), 
+	                                  React.createElement("option", {value: "tuesday"}, " Tuesday"), 
+	                                  React.createElement("option", {value: "wednesday"}, " Wednesday"), 
+	                                  React.createElement("option", {value: "thursday"}, " Thursday"), 
+	                                  React.createElement("option", {value: "friday"}, " Friday"), 
+	                                  React.createElement("option", {value: "saturday"}, " Saturday"), 
+	                                  React.createElement("option", {value: "sunday"}, " Sunday")
+	                                  ), " ", React.createElement("space", null), 
+	          "Times available: ", React.createElement("space", null, " "), 
+	                      React.createElement("select", {name: "time", id: "time"}, 
+	                        React.createElement("option", {value: "-"}), 
+	                        React.createElement("option", {value: "5:00 AM"}, "5:00 AM"), 
+	                        React.createElement("option", {value: "5:15 AM"}, "5:15 AM"), 
+	                        React.createElement("option", {value: "5:30 AM"}, "5:30 AM"), 
+	                        React.createElement("option", {value: "5:45 AM"}, "5:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "6:00 AM"}, "6:00 AM"), 
+	                        React.createElement("option", {value: "6:15 AM"}, "6:15 AM"), 
+	                        React.createElement("option", {value: "6:30 AM"}, "6:30 AM"), 
+	                        React.createElement("option", {value: "6:45 AM"}, "6:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "7:00 AM"}, "7:00 AM"), 
+	                        React.createElement("option", {value: "7:15 AM"}, "7:15 AM"), 
+	                        React.createElement("option", {value: "7:30 AM"}, "7:30 AM"), 
+	                        React.createElement("option", {value: "7:45 AM"}, "7:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "8:00 AM"}, "8:00 AM"), 
+	                        React.createElement("option", {value: "8:15 AM"}, "8:15 AM"), 
+	                        React.createElement("option", {value: "8:30 AM"}, "8:30 AM"), 
+	                        React.createElement("option", {value: "8:45 AM"}, "8:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "9:00 AM"}, "9:00 AM"), 
+	                        React.createElement("option", {value: "9:15 AM"}, "9:15 AM"), 
+	                        React.createElement("option", {value: "9:30 AM"}, "9:30 AM"), 
+	                        React.createElement("option", {value: "9:45 AM"}, "9:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "10:00 AM"}, "10:00 AM"), 
+	                        React.createElement("option", {value: "10:15 AM"}, "10:15 AM"), 
+	                        React.createElement("option", {value: "10:30 AM"}, "10:30 AM"), 
+	                        React.createElement("option", {value: "10:45 AM"}, "10:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "11:00 AM"}, "11:00 AM"), 
+	                        React.createElement("option", {value: "11:15 AM"}, "11:15 AM"), 
+	                        React.createElement("option", {value: "11:30 AM"}, "11:30 AM"), 
+	                        React.createElement("option", {value: "11:45 AM"}, "11:45 AM"), 
+	                       
+	                        React.createElement("option", {value: "12:00 PM"}, "12:00 PM"), 
+	                        React.createElement("option", {value: "12:15 PM"}, "12:15 PM"), 
+	                        React.createElement("option", {value: "12:30 PM"}, "12:30 PM"), 
+	                        React.createElement("option", {value: "12:45 PM"}, "12:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "1:00 PM"}, "1:00 PM"), 
+	                        React.createElement("option", {value: "1:15 PM"}, "1:15 PM"), 
+	                        React.createElement("option", {value: "1:30 PM"}, "1:30 PM"), 
+	                        React.createElement("option", {value: "1:45 PM"}, "1:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "2:00 PM"}, "2:00 PM"), 
+	                        React.createElement("option", {value: "2:15 PM"}, "2:15 PM"), 
+	                        React.createElement("option", {value: "2:30 PM"}, "2:30 PM"), 
+	                        React.createElement("option", {value: "2:45 PM"}, "2:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "3:00 PM"}, "3:00 PM"), 
+	                        React.createElement("option", {value: "3:15 PM"}, "3:15 PM"), 
+	                        React.createElement("option", {value: "3:30 PM"}, "3:30 PM"), 
+	                        React.createElement("option", {value: "3:45 PM"}, "3:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "4:00 PM"}, "4:00 PM"), 
+	                        React.createElement("option", {value: "4:15 PM"}, "4:15 PM"), 
+	                        React.createElement("option", {value: "4:30 PM"}, "4:30 PM"), 
+	                        React.createElement("option", {value: "4:45 PM"}, "4:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "5:00 PM"}, "5:00 PM"), 
+	                        React.createElement("option", {value: "5:15 PM"}, "5:15 PM"), 
+	                        React.createElement("option", {value: "5:30 PM"}, "5:30 PM"), 
+	                        React.createElement("option", {value: "5:45 PM"}, "5:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "6:00 PM"}, "6:00 PM"), 
+	                        React.createElement("option", {value: "6:15 PM"}, "6:15 PM"), 
+	                        React.createElement("option", {value: "6:30 PM"}, "6:30 PM"), 
+	                        React.createElement("option", {value: "6:45 PM"}, "6:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "7:00 PM"}, "7:00 PM"), 
+	                        React.createElement("option", {value: "7:15 PM"}, "7:15 PM"), 
+	                        React.createElement("option", {value: "7:30 PM"}, "7:30 PM"), 
+	                        React.createElement("option", {value: "7:45 PM"}, "7:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "8:00 PM"}, "8:00 PM"), 
+	                        React.createElement("option", {value: "8:15 PM"}, "8:15 PM"), 
+	                        React.createElement("option", {value: "8:30 PM"}, "8:30 PM"), 
+	                        React.createElement("option", {value: "8:45 PM"}, "8:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "9:00 PM"}, "9:00 PM"), 
+	                        React.createElement("option", {value: "9:15 PM"}, "9:15 PM"), 
+	                        React.createElement("option", {value: "9:30 PM"}, "9:30 PM"), 
+	                        React.createElement("option", {value: "9:45 PM"}, "9:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "10:00 PM"}, "10:00 PM"), 
+	                        React.createElement("option", {value: "10:15 PM"}, "10:15 PM"), 
+	                        React.createElement("option", {value: "10:30 PM"}, "10:30 PM"), 
+	                        React.createElement("option", {value: "10:45 PM"}, "10:45 PM"), 
+	                       
+	                        React.createElement("option", {value: "11:00 PM"}, "11:00 PM"), 
+	                        React.createElement("option", {value: "11:15 PM"}, "11:15 PM"), 
+	                        React.createElement("option", {value: "11:30 PM"}, "11:30 PM"), 
+	                        React.createElement("option", {value: "11:45 PM"}, "11:45 PM")
+	                      ), React.createElement("space", null, " "), 
+	          React.createElement("button", {onClick: this.addNewTime}, " Add another time"), 
+	          React.createElement("div", {id: "time"}
+
+	          ), 
+	            React.createElement("br", null), React.createElement("br", null), React.createElement("button", {onClick: this.handleClick}, 
+	            "Submit"
+	            )
+	          )
+	      
 
 	      );
 	    }
--- app/js/app.js
+++ app/js/app.js
@@ -13,7 +13,7 @@ var transitionTo = Router.transitionTo;
 var profileDriveways = '';
 var allDriveways = [];
 var userDriveways = [];
-
+console.log('hello');
 function get(parameter)
 { 
   var url = window.location.href;
@@ -155,7 +155,7 @@ var driveway = React.createClass
       return {address: address, numCars: numCars, zip: zip, city: city, state: state, editing: true};
     }
     else
-        return {address: '', numCars: '1', zip:'', city: '', state:'AL', editing: false};
+        return {address: '', numCars: '1', zip:'', city: '', state:'AL', editing: false, time: []};
   },
   handleChange: function(event) 
   {
@@ -173,6 +173,13 @@ var driveway = React.createClass
   handleClick: function(event)
   {
     drivewayDAO.add(localStorage.username, this.state.address, this.state.numCars, this.state.city, this.state.zip, this.state.state);
+  },
+  addNewTime: function()
+  {
+     var node = document.getElementById("time");
+var textnode = document.createTextNode("Water");         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+document.getElementById("time").appendChild(node); 
   },
   remove: function()
   {
@@ -256,10 +263,125 @@ var driveway = React.createClass
                         <option value='2'>2</option>
                         <option value='3'>3</option>
                       </select>
-        <br/><br/><button onClick={this.handleClick}>
-        Submit
-        </button>
-      </div>
+
+          <br/><br/>
+          Day: <space> </space> <select name="day" id="day">
+                                  <option value=""> </option>
+                                  <option value="monday"> Monday</option>
+                                  <option value="tuesday"> Tuesday</option>
+                                  <option value="wednesday"> Wednesday</option>
+                                  <option value="thursday"> Thursday</option>
+                                  <option value="friday"> Friday</option>
+                                  <option value="saturday"> Saturday</option>
+                                  <option value="sunday"> Sunday</option>
+                                  </select> <space></space>
+          Times available: <space> </space>
+                      <select name="time" id="time">
+                        <option value="-"></option>
+                        <option value="5:00 AM">5:00 AM</option>
+                        <option value="5:15 AM">5:15 AM</option>
+                        <option value="5:30 AM">5:30 AM</option>
+                        <option value="5:45 AM">5:45 AM</option>
+                       
+                        <option value="6:00 AM">6:00 AM</option>
+                        <option value="6:15 AM">6:15 AM</option>
+                        <option value="6:30 AM">6:30 AM</option>
+                        <option value="6:45 AM">6:45 AM</option>
+                       
+                        <option value="7:00 AM">7:00 AM</option>
+                        <option value="7:15 AM">7:15 AM</option>
+                        <option value="7:30 AM">7:30 AM</option>
+                        <option value="7:45 AM">7:45 AM</option>
+                       
+                        <option value="8:00 AM">8:00 AM</option>
+                        <option value="8:15 AM">8:15 AM</option>
+                        <option value="8:30 AM">8:30 AM</option>
+                        <option value="8:45 AM">8:45 AM</option>
+                       
+                        <option value="9:00 AM">9:00 AM</option>
+                        <option value="9:15 AM">9:15 AM</option>
+                        <option value="9:30 AM">9:30 AM</option>
+                        <option value="9:45 AM">9:45 AM</option>
+                       
+                        <option value="10:00 AM">10:00 AM</option>
+                        <option value="10:15 AM">10:15 AM</option>
+                        <option value="10:30 AM">10:30 AM</option>
+                        <option value="10:45 AM">10:45 AM</option>
+                       
+                        <option value="11:00 AM">11:00 AM</option>
+                        <option value="11:15 AM">11:15 AM</option>
+                        <option value="11:30 AM">11:30 AM</option>
+                        <option value="11:45 AM">11:45 AM</option>
+                       
+                        <option value="12:00 PM">12:00 PM</option>
+                        <option value="12:15 PM">12:15 PM</option>
+                        <option value="12:30 PM">12:30 PM</option>
+                        <option value="12:45 PM">12:45 PM</option>
+                       
+                        <option value="1:00 PM">1:00 PM</option>
+                        <option value="1:15 PM">1:15 PM</option>
+                        <option value="1:30 PM">1:30 PM</option>
+                        <option value="1:45 PM">1:45 PM</option>
+                       
+                        <option value="2:00 PM">2:00 PM</option>
+                        <option value="2:15 PM">2:15 PM</option>
+                        <option value="2:30 PM">2:30 PM</option>
+                        <option value="2:45 PM">2:45 PM</option>
+                       
+                        <option value="3:00 PM">3:00 PM</option>
+                        <option value="3:15 PM">3:15 PM</option>
+                        <option value="3:30 PM">3:30 PM</option>
+                        <option value="3:45 PM">3:45 PM</option>
+                       
+                        <option value="4:00 PM">4:00 PM</option>
+                        <option value="4:15 PM">4:15 PM</option>
+                        <option value="4:30 PM">4:30 PM</option>
+                        <option value="4:45 PM">4:45 PM</option>
+                       
+                        <option value="5:00 PM">5:00 PM</option>
+                        <option value="5:15 PM">5:15 PM</option>
+                        <option value="5:30 PM">5:30 PM</option>
+                        <option value="5:45 PM">5:45 PM</option>
+                       
+                        <option value="6:00 PM">6:00 PM</option>
+                        <option value="6:15 PM">6:15 PM</option>
+                        <option value="6:30 PM">6:30 PM</option>
+                        <option value="6:45 PM">6:45 PM</option>
+                       
+                        <option value="7:00 PM">7:00 PM</option>
+                        <option value="7:15 PM">7:15 PM</option>
+                        <option value="7:30 PM">7:30 PM</option>
+                        <option value="7:45 PM">7:45 PM</option>
+                       
+                        <option value="8:00 PM">8:00 PM</option>
+                        <option value="8:15 PM">8:15 PM</option>
+                        <option value="8:30 PM">8:30 PM</option>
+                        <option value="8:45 PM">8:45 PM</option>
+                       
+                        <option value="9:00 PM">9:00 PM</option>
+                        <option value="9:15 PM">9:15 PM</option>
+                        <option value="9:30 PM">9:30 PM</option>
+                        <option value="9:45 PM">9:45 PM</option>
+                       
+                        <option value="10:00 PM">10:00 PM</option>
+                        <option value="10:15 PM">10:15 PM</option>
+                        <option value="10:30 PM">10:30 PM</option>
+                        <option value="10:45 PM">10:45 PM</option>
+                       
+                        <option value="11:00 PM">11:00 PM</option>
+                        <option value="11:15 PM">11:15 PM</option>
+                        <option value="11:30 PM">11:30 PM</option>
+                        <option value="11:45 PM">11:45 PM</option>
+                      </select><space> </space>
+          <button onClick={this.addNewTime}> Add another time</button>
+          <div id="time">
+
+          </div>
+            <br/><br/><button onClick={this.handleClick}>
+            Submit
+            </button>
+          </div>
+      
 
       );
     }
