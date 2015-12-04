d2bbf3716bf33368988537dfa880c49b62ac4ea6
Brennen Barney <brennen.barney@gmail.com>
2015-12-04 00:17:59 +0000

    added cb info in stripepayment

--- app/js/components/StripePayment.js
+++ app/js/components/StripePayment.js
@@ -23,6 +23,10 @@ var reservationDuration;
 var reservationTime;
 var city;
 var tokenId ="null";
+var Name;
+var Email;
+var CardType;
+var Last4;
 
 var StripeButton = React.createClass({
     mixins: [ReactScriptLoaderMixin, History, Lifecycle],
@@ -57,6 +61,10 @@ var StripeButton = React.createClass({
                   var reservationDuration2 = reservationDuration;
                   var reservationTime2 = reservationTime;
                   var city2 = city;
+                  Name = token.card.name;
+                  Email = token.email;
+                  CardType = token.card.brand;
+                  Last4 = token.card.Last4;
                     $.ajax
                     ({
                         url: url,
@@ -96,6 +104,17 @@ var StripeButton = React.createClass({
                         var data1 = {Price: priceT};
                         tokenId = "null";
                         localStorage.price = priceT;
+                        localStorage.Name = Name;
+                        localStorage.ResAddress = streetAddress;
+                        localStorage.State = state;
+                        localStorage.City = city;
+                        localStorage.Zip = zip;
+                        localStorage.ResDate = reservationDate;
+                        localStorage.ResDuration = reservationDuration;
+                        localStorage.ResTime = reservationTime;
+                        localStorage.email = Email;
+                        localStorage.cardType = CardType;
+                        localStorage.Last4 = Last4;
 
                         self.history.pushState(null,'/confirm');
                     }
