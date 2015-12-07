c0ae894fa56dce7ecdd62a8c05a0f23a67e756e5
Brennen Barney <brennen.barney@gmail.com>
2015-12-07 06:37:27 +0000

    fixed port

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var api = require('./models/api.js');
 
 
 // start the server
-var server = app.listen(4040, function() {
-console.log("Started on port 4040");
+var server = app.listen(3000, function() {
+console.log("Started on port 3000");
 var host = server.address().address;
 var port = server.address().port;
 });
