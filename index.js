987182f0a95b88ce466efe1c7b4665d849be0e3c
Brennen Barney <brennen.barney@gmail.com>
2015-12-10 02:29:02 +0000

    changed port back

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var api = require('./models/api.js');
 
 
 // start the server
-var server = app.listen(3070, function() {
-console.log("Started on port 3070");
+var server = app.listen(3000, function() {
+console.log("Started on port 3000");
 var host = server.address().address;
 var port = server.address().port;
 });
